d0ba151f1689d6d93deb20b8ce11257eb6b6388e
Parin Vachhani <vachhaniparin@gmail.com>
2016-09-12 17:26:32 +0530

    Basic table storage done. Basic parsing of SQL queries using pyparsing to extract table and field names done.

--- sql-engine.py
+++ sql-engine.py
@@ -1,5 +1,4 @@
 import csv
-import sqlparse
 # import sys
 
 table_list = []
@@ -13,7 +12,7 @@ for line in lines:
     if word in line:
         table_found = True
 lines.close()
-print table_list
+# print table_list
 
 table_start = False
 field_start = False
@@ -33,12 +32,12 @@ for line in lines:
     if line.strip() in table_list:
         field_start = True
 lines.close()
-print field_list
+# print field_list
 
 table_dict = {}
 for i in range(len(table_list)):
     table_dict[table_list[i]] = field_list[i]
-print table_dict
+# print table_dict
 
 table_field = {}
 for table in table_list:
@@ -51,9 +50,103 @@ for table in table_list:
             table_field[table].append(row)
             # print row
     # print table_field[table]
-for table in table_field:
-    print table
-    for row in table_field[table]:
-        print row
+# for table in table_field:
+#     print table
+#     for row in table_field[table]:
+#         print row
 
-print(sqlparse.format('select * from foo', reindent=True))
+# print table_field
+
+##########################################################################
+from pyparsing import Literal, CaselessLiteral, Word, Upcase, delimitedList, Optional, \
+    Combine, Group, alphas, nums, alphanums, ParseException, Forward, oneOf, quotedString, \
+    ZeroOrMore, restOfLine, Keyword
+
+def test( str ):
+    print str,"->"
+    try:
+        tokens = simpleSQL.parseString( str )
+        print "tokens = ",        tokens
+        print "tokens.columns =", tokens.columns
+        print "tokens.tables =",  tokens.tables
+        print "tokens.where =", tokens.where
+    except ParseException, err:
+        print " "*err.loc + "^\n" + err.msg
+        print err
+    print
+
+
+# define SQL tokens
+selectStmt = Forward()
+selectToken = Keyword("select", caseless=True)
+fromToken   = Keyword("from", caseless=True)
+
+ident          = Word( alphas, alphanums + "_$" ).setName("identifier")
+columnName     = Upcase( delimitedList( ident, ".", combine=True ) )
+columnNameList = Group( delimitedList( columnName ) )
+tableName      = Upcase( delimitedList( ident, ".", combine=True ) )
+tableNameList  = Group( delimitedList( tableName ) )
+
+whereExpression = Forward()
+and_ = Keyword("and", caseless=True)
+or_ = Keyword("or", caseless=True)
+in_ = Keyword("in", caseless=True)
+
+E = CaselessLiteral("E")
+binop = oneOf("= != < > >= <= eq ne lt le gt ge", caseless=True)
+arithSign = Word("+-",exact=1)
+realNum = Combine( Optional(arithSign) + ( Word( nums ) + "." + Optional( Word(nums) )  |
+                                                         ( "." + Word(nums) ) ) + 
+            Optional( E + Optional(arithSign) + Word(nums) ) )
+intNum = Combine( Optional(arithSign) + Word( nums ) + 
+            Optional( E + Optional("+") + Word(nums) ) )
+
+columnRval = realNum | intNum | quotedString | columnName # need to add support for alg expressions
+whereCondition = Group(
+    ( columnName + binop + columnRval ) |
+    ( columnName + in_ + "(" + delimitedList( columnRval ) + ")" ) |
+    ( columnName + in_ + "(" + selectStmt + ")" ) |
+    ( "(" + whereExpression + ")" )
+    )
+whereExpression << whereCondition + ZeroOrMore( ( and_ | or_ ) + whereExpression ) 
+
+# define the grammar
+selectStmt      << ( selectToken + 
+                   ( '*' | columnNameList ).setResultsName( "columns" ) + 
+                   fromToken + 
+                   tableNameList.setResultsName( "tables" ) + 
+                   Optional( Group( CaselessLiteral("where") + whereExpression ), "" ).setResultsName("where") )
+
+simpleSQL = selectStmt
+
+# define Oracle comment format, and ignore them
+oracleSqlComment = "--" + restOfLine
+simpleSQL.ignore( oracleSqlComment )
+
+##########################################################################
+def field_extract ( table, field):
+    field_output = []
+    for t in table_field:
+    	if t == table:
+    		for record in table_field[t]:
+    			if field in record:
+    				field_output.append(record[field])
+    print field_output
+
+field_extract('table1','A')
+
+##########################################################################
+
+print "-------------------------------------------------"
+print "Welcome to mini SQL engine!"
+print "Enter an SQL query to run it on given database"
+print "Enter 'quit','q' or 'exit' to exit the program"
+print "-------------------------------------------------"
+
+while True:
+    query = raw_input("Input SQL query:\n")
+    if query == "quit" or query == "q" or query == "exit":
+        break
+    else:
+        print "->\n"
+        print test(query)
